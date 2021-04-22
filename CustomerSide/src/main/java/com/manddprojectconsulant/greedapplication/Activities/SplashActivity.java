821d5d1d9caaff89f65b2cad4a71f648d8d383a8
Mitul Jayswal <tarajayswal96@gmail.com>
2021-04-22 20:37:42 +0530

    Master commit

---
+++ CustomerSide/src/main/java/com/manddprojectconsulant/greedapplication/Activities/SplashActivity.java
@@ -0,0 +1,114 @@
+package com.manddprojectconsulant.greedapplication.Activities;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.view.ViewCompat;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.manddprojectconsulant.greedapplication.R;
+
+import java.util.List;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView logo_image;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setExitTransition(null);
+        setContentView(R.layout.activity_splash);
+
+        requestStoragePermission();
+        
+
+        //ID's
+        logo_image = findViewById(R.id.logo_image);
+        permissioncheck();
+
+
+
+    }
+
+    private void permissioncheck() {
+
+        String transitionName = ViewCompat.getTransitionName(logo_image);
+        Intent intent = new Intent(this, LoginActivity.class);
+        ActivityOptionsCompat options =
+                ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        this, logo_image, transitionName);
+
+        logo_image.postDelayed(() -> {
+            startActivity(intent, options.toBundle());
+            ActivityCompat.finishAfterTransition(this);
+        }, 3000);
+
+
+    }
+
+
+    private void requestStoragePermission() {
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        // check if all permissions are granted
+                        if (report.areAllPermissionsGranted()) {
+
+
+                            Toast.makeText(getApplicationContext(), "All permissions are granted!", Toast.LENGTH_SHORT).show();
+
+
+
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                }).
+                withErrorListener(new PermissionRequestErrorListener() {
+                    @Override
+                    public void onError(DexterError error) {
+                        Toast.makeText(getApplicationContext(), "Error occurred! ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .onSameThread()
+                .check();
+    }
+
+
+
+
+
+
+}
---
+++ CustomerSide/src/main/java/com/manddprojectconsulant/greedapplication/Adapter/AdapterforCategory.java
@@ -0,0 +1,135 @@
+package com.manddprojectconsulant.greedapplication.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.chrisbanes.photoview.PhotoView;
+import com.github.florent37.materialimageloading.MaterialImageLoading;
+import com.manddprojectconsulant.greedapplication.Activities.DashboardActivity;
+import com.manddprojectconsulant.greedapplication.Activities.FestivalActivity.DiwaliGalleryActivity;
+import com.manddprojectconsulant.greedapplication.Activities.FestivalActivity.ChristmasGalleryActivity;
+import com.manddprojectconsulant.greedapplication.Activities.FestivalActivity.HoliGalleryActivity;
+import com.manddprojectconsulant.greedapplication.Activities.FestivalActivity.JanmashtamiGalleryActivity;
+import com.manddprojectconsulant.greedapplication.Model.ModelforCategory;
+import com.manddprojectconsulant.greedapplication.R;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class AdapterforCategory extends RecyclerView.Adapter<AdapterforCategory.Myholder> {
+
+    List<ModelforCategory> list;
+    Context context;
+
+    public AdapterforCategory(DashboardActivity context, List<ModelforCategory> list) {
+
+        this.context = context;
+        this.list = list;
+
+    }
+
+    @NonNull
+    @Override
+    public Myholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.designforvideo, parent, false);
+
+        return new Myholder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull Myholder holder, int position) {
+
+
+        //holder.titleView.setHint(list.get(position).getTitle());
+        Picasso.get()
+                .load(list.get(position).getImageforthumbail())
+                .resize(150, 100)
+                .centerCrop()
+                .into(holder.imageviewforthumbail, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        MaterialImageLoading.animate(holder.imageviewforthumbail).setDuration(2000).start();
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+
+                    }
+                });
+
+
+        holder.imageviewforthumbail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String title = list.get(position).getTitle();
+
+
+                if (title.equals("Happy Diwali")) {
+
+                    Intent diwali = new Intent(context, DiwaliGalleryActivity.class);
+                    context.startActivity(diwali);
+
+                }
+                if (title.equals("Happy Holi")) {
+
+                    Intent holi = new Intent(context, HoliGalleryActivity.class);
+                    context.startActivity(holi);
+
+
+                }
+                if (title.equals("Merry Christmas")) {
+
+                    Intent christmas = new Intent(context, ChristmasGalleryActivity.class);
+                    context.startActivity(christmas);
+
+                }
+                if (title.equals("Happy Janmashtami")) {
+                    Intent krishna = new Intent(context, JanmashtamiGalleryActivity.class);
+                    context.startActivity(krishna);
+
+
+                }
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class Myholder extends RecyclerView.ViewHolder {
+
+        ImageView imageviewforthumbail;
+        // TextView titleView;
+
+        public Myholder(@NonNull View itemView) {
+            super(itemView);
+
+            imageviewforthumbail = itemView.findViewById(R.id.imageforthumbnail);
+            //titleView = itemView.findViewById(R.id.title_text);
+
+
+        }
+    }
+
+
+}
